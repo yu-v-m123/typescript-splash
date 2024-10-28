a1cbb2daa973ac6439bd97f5efb891e3a4466a90
yu <nagase.yuki0123@gmail.com>
2024-10-29 01:01:10 +0900

    fix: replace ImageModal with typescript

---
+++ src/components/ImageModal.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Modal } from 'react-bootstrap';
+import './ImageModal.css';
+import { ImageCardProps } from './ImageCard';
+
+type Props = {
+  image: ImageCardProps;
+  size?: 'sm' | 'lg' | 'xl' | undefined;
+  show: boolean;
+  onHide: () => void;
+}
+
+const ImageModal: React.FC<Props> = (props) => {
+  return (
+    <Modal
+      {...props}
+      size={"lg"}
+      centered>
+      <Modal.Body>
+        <img className="ImageSize" alt={props.image.description} src={props.image.urls.regular} />
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ImageModal;
